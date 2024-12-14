6829dd62000d8b75ff5d91af400f2fe1a97f89fb
mysterygothic <waleedlaith393@gmail.com>
2024-12-14 07:33:43 -0500

    Update index.tsx

--- patches/src/userplugins/betterMicrophone.desktop/index.tsx
+++ patches/src/userplugins/betterMicrophone.desktop/index.tsx
@@ -1,24 +1,5 @@
-/*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2023 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 import { PluginAuthor, PluginDef } from "@utils/types";
-
-import { addSettingsPanelButton, Emitter, MicrophoneSettingsIcon, removeSettingsPanelButton } from "../philsPluginLibrary";
+import { Emitter } from "../philsPluginLibrary";
 import { PluginInfo } from "./constants";
 import { openMicrophoneSettingsModal } from "./modals";
 import { MicrophonePatcher } from "./patchers";
@@ -32,11 +13,20 @@ export default new class Plugin implements PluginDef {
 
     public microphonePatcher?: MicrophonePatcher;
 
+    // Adding the toolbox 
+    toolboxActions: Record<string, () => void>;
+
     constructor() {
         this.name = PluginInfo.PLUGIN_NAME;
         this.description = PluginInfo.DESCRIPTION;
         this.authors = [PluginInfo.AUTHOR, ...Object.values(PluginInfo.CONTRIBUTORS)] as PluginAuthor[];
         this.dependencies = ["PhilsPluginLibrary"];
+
+        // actions for the toolbox 
+        this.toolboxActions = {
+            "Open Microphone Settings": openMicrophoneSettingsModal,
+            "Toggle Microphone Patcher": this.toggleMicrophonePatcher.bind(this)
+        };
     }
 
     start(): void {
@@ -44,14 +34,24 @@ export default new class Plugin implements PluginDef {
 
         this.microphonePatcher = new MicrophonePatcher().patch();
 
-        addSettingsPanelButton({ name: PluginInfo.PLUGIN_NAME, icon: MicrophoneSettingsIcon, tooltipText: "Microphone Settings", onClick: openMicrophoneSettingsModal });
+        
     }
 
     stop(): void {
         this.microphonePatcher?.unpatch();
 
         Emitter.removeAllListeners(PluginInfo.PLUGIN_NAME);
+    }
 
-        removeSettingsPanelButton(PluginInfo.PLUGIN_NAME);
+    
+    private toggleMicrophonePatcher(): void {
+        if (this.microphonePatcher) {
+            this.microphonePatcher.unpatch();
+            this.microphonePatcher = undefined;
+            console.log(`${this.name}: Microphone patcher disabled.`);
+        } else {
+            this.microphonePatcher = new MicrophonePatcher().patch();
+            console.log(`${this.name}: Microphone patcher enabled.`);
+        }
     }
 };
